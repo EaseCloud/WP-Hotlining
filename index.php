6f12bd62a804a5fa4b7bf8ba6e192a55c4aebf35
fish_ball <57082212@qq.com>
2016-01-17 13:30:22 +0800

    完成了HardCoding版本

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ Text Domain: wp_hot_linking
 
 define('WHL_DOMAIN', 'wp_hot_linking');
 define('WHL_OPTION_PAGE', 'options-hot-linking');
+define('WHL_VERSION', '0.1');
 
 //var_dump(add_query_arg(array('action' => 'wechat_callback'), home_url('/wp-admin/admin-ajax.php')));
 
@@ -46,7 +47,7 @@ add_action('admin_menu', function () {
             <h2><?php _e('Hot-linking Settings', WHL_DOMAIN); ?></h2>
             <form method="post" action="options.php">
                 <?php settings_fields(WHL_DOMAIN); ?>
-                <?php do_settings_sections(WHL_DOMAIN); ?>
+                <?php do_settings_sections(WHL_OPTION_PAGE); ?>
                 <?php submit_button(); ?>
             </form>
         </div><?php
@@ -80,6 +81,8 @@ add_action('admin_init', function() {
     $setting_fields = array(
         'whl_domains' => array(
             'title' => 'Domains',
+            'type' => 'textarea',
+            'class' => 'regular-text code',
             'description' => 'Mark the Hot-linking domains.',
         ),
     );
@@ -111,15 +114,25 @@ add_action('admin_init', function() {
         $field_type = @$args['type'] ?: 'text';
         $field_class = @$args['class'] ?: 'regular-text ltr';
         $field_description = @$args['description'] ?: '';
-        ?>
-        <input class="<?php echo $field_class; ?>"
-               type="<?php echo $field_type;?>"
-               id="<?php echo $field_name;?>"
-               value="<?php form_option($field_name);?>"
-               name="<?php echo $field_name;?>"
-               aria-describedby="<?php echo $field_name;?>-description"
-               <?php echo @$args['readonly'] ? 'readonly' : ''; ?>
+        if($field_type == 'textarea') { ?>
+            <textarea class="<?php echo $field_class; ?>"
+                   type="<?php echo $field_type;?>"
+                   id="<?php echo $field_name;?>"
+                   name="<?php echo $field_name;?>"
+                   rows="8"
+                   aria-describedby="<?php echo $field_name;?>-description"
+                <?php echo @$args['readonly'] ? 'readonly' : ''; ?>
+            ><?php form_option($field_name);?></textarea>
+        <?php } elseif($field_type == 'text') { ?>
+            <input class="<?php echo $field_class; ?>"
+                   type="<?php echo $field_type;?>"
+                   id="<?php echo $field_name;?>"
+                   value="<?php form_option($field_name);?>"
+                   name="<?php echo $field_name;?>"
+                   aria-describedby="<?php echo $field_name;?>-description"
+                <?php echo @$args['readonly'] ? 'readonly' : ''; ?>
             />
+        <?php }?>
         <p class="description"
            id="<?php echo $field_name;?>-description"><?php
             echo $field_description;
@@ -128,4 +141,44 @@ add_action('admin_init', function() {
     }
 
 });
+
+
+/**
+ * 加入前端的替换链接
+ */
+add_action('wp_enqueue_scripts', function() {
+    $hash = md5(get_option('whl_domains'));
+    wp_enqueue_script(
+        'wp-hotlinking',                                // handle
+        plugin_dir_url(__FILE__)
+            ."js/hotlinking.js?hash=$hash",            // source
+        array('jquery'),                                // dependence
+        WHL_VERSION,                                    // version
+        true                                            // in_footer
+    );
+});
+
+
+add_action('wp_ajax_hotlink_img', 'wp_ajax_hotlink_img_callback');
+add_action('wp_ajax_nopriv_hotlink_img', 'wp_ajax_hotlink_img_callback');
+function wp_ajax_hotlink_img_callback() {
+    $url = $_GET['url'];
+    if(preg_match('/^https?:\\/\\//', $url)) {
+//        $result = (file_get_contents($url));
+//        exit($result);
+        exit(grab_image($url));
+    }
+}
+
+function grab_image($url){
+    $ch = curl_init ($url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    $raw=curl_exec($ch);
+    curl_close ($ch);
+    return $raw;
+}
+
 
---
+++ js/hotlinking.js
@@ -0,0 +1,22 @@
+jQuery(function($) {
+    var whl_domains = [
+        'http://\\w\\.hiphotos\\.baidu\\.com'
+    ];
+    var source_url = '/wp-admin/admin-ajax.php?action=hotlink_img&url=';
+
+    //var i, domain;
+    //for(i = 0; i < whl_domains.length; ++i) {
+    //    domain = whl_domains[i];
+    //    $('img[src^="'+domain+'"]').each(function() {
+    //        $(this).attr('src', source_url + $(this).attr('src'));
+    //    });
+    //}
+    $('img').each(function() {
+        var url = $(this).attr('src') || '';
+        var target = source_url + url;
+        $.each(whl_domains, function(index, domain) {
+            if((new RegExp(domain)).test(url)) url = '~~~';
+        });
+        if(url === '~~~') $(this).attr('src', target);
+    });
+});
